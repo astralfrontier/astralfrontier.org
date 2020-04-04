6f8560f1e6a9cdd66731fe037f9413926d7fdce5
Bill Garrett <garrett@astralfrontier.org>
2020-04-04 09:09:49 -0700

    Show extra categories in a dropdown

--- src/components/category-navs.tsx
+++ src/components/category-navs.tsx
@@ -1,7 +1,24 @@
 import React from "react"
-import { useStaticQuery, graphql } from "gatsby"
+import { useStaticQuery, graphql, Link } from "gatsby"
+import Dropdown from "react-bootstrap/Dropdown"
 import Nav from "react-bootstrap/Nav"
-import { append, countBy, flatten, identity, isNil, keys, map, path, reduce, reject, sortBy } from 'ramda'
+import NavItem from "react-bootstrap/NavItem"
+import NavLink from "react-bootstrap/NavLink"
+import {
+  append,
+  countBy,
+  flatten,
+  identity,
+  isNil,
+  keys,
+  map,
+  path,
+  reduce,
+  reject,
+  sortBy,
+  take,
+  drop,
+} from "ramda"
 
 /**
  * Find all categories used in the site.
@@ -10,31 +27,67 @@ import { append, countBy, flatten, identity, isNil, keys, map, path, reduce, rej
  * TODO: Return the first five as navs, followed by a dropdown containing the remaining categories.
  */
 
+const MAX_CATEGORIES = 5
+
 export default () => {
   const data = useStaticQuery(graphql`
-  {
-    allMarkdownRemark {
-      nodes {
-        fields {
-          category
+    {
+      allMarkdownRemark {
+        nodes {
+          fields {
+            category
+          }
         }
       }
     }
-  }  
   `)
 
-  const categories = flatten(map(path(['fields', 'category']), data.allMarkdownRemark.nodes))
+  const categories = flatten(
+    map(path(["fields", "category"]), data.allMarkdownRemark.nodes)
+  )
   const catCounts = countBy(identity, reject(isNil, categories))
-  const catCountsArray = reduce((a, k) => append([k, catCounts[k]], a), [], keys(catCounts))
-  const sortedCatCounts = map(e => e[0], sortBy(e => -e[1], catCountsArray))
+  const catCountsArray = reduce(
+    (a, k) => append([k, catCounts[k]], a),
+    [],
+    keys(catCounts)
+  )
+  const sortedCatCounts = map(
+    e => e[0],
+    sortBy(e => -e[1], catCountsArray)
+  )
 
   return (
     <>
-    {map(category => (
-      <Nav.Item>
-        <Nav.Link className={'small-caps'} href={`/category/${category}`}>{category}</Nav.Link>
-      </Nav.Item>
-    ), sortedCatCounts)}
+      {map(
+        category => (
+          <Nav.Item>
+            <Nav.Link className={"small-caps"} href={`/category/${category}`}>
+              {category}
+            </Nav.Link>
+          </Nav.Item>
+        ),
+        take(MAX_CATEGORIES, sortedCatCounts)
+      )}
+      {sortedCatCounts.length > MAX_CATEGORIES && (
+        <Dropdown as={NavItem}>
+          <Dropdown.Toggle as={NavLink}>more categories</Dropdown.Toggle>
+          <Dropdown.Menu>
+            {map(
+              category => (
+                <Dropdown.Item>
+                  <Link
+                    className={"small-caps"}
+                    to={`/category/${category}`}
+                  >
+                    {category}
+                  </Link>
+                </Dropdown.Item>
+              ),
+              drop(MAX_CATEGORIES, sortedCatCounts)
+            )}
+          </Dropdown.Menu>
+        </Dropdown>
+      )}
     </>
   )
 }
