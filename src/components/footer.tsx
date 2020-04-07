cead18f0010bc3385f1e57d6718f027391355836
Bill Garrett <garrett@astralfrontier.org>
2020-04-06 21:02:45 -0700

    Round 2 usability improvements

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,7 +1,10 @@
 import React from "react"
-import Navbar from "react-bootstrap/Navbar"
-import Nav from "react-bootstrap/Nav"
+import Container from "react-bootstrap/Container"
+import Row from "react-bootstrap/Row"
+import Col from "react-bootstrap/Col"
 import { useStaticQuery, graphql } from "gatsby"
+
+import TagCloud from './tag-cloud'
 
 const Footer = _props => {
   const data = useStaticQuery(graphql`
@@ -14,17 +17,19 @@ const Footer = _props => {
     }
   `)
   return (
-    <Navbar variant="dark" bg="dark">
-      <Navbar.Brand>&copy; 2020 @{data.site.siteMetadata.author}</Navbar.Brand>
-      <Navbar.Toggle />
-      <Navbar.Collapse id="footer-nav">
-        <Nav className={'justify-content-end ml-auto'}>
-          <Nav.Item>
-            <Nav.Link href={`https://twitter.com/${data.site.siteMetadata.author}`}>Twitter</Nav.Link>
-          </Nav.Item>
-        </Nav>
-      </Navbar.Collapse>
-    </Navbar>
+    <Container fluid>
+      <Row>
+        <Col className="footer-identification">
+          &copy; 2020 @{data.site.siteMetadata.author}
+        </Col>
+        <Col className="footer-tag-cloud">
+          <TagCloud />
+        </Col>
+        <Col className="footer-links">
+          <a href={`https://twitter.com/${data.site.siteMetadata.author}`}>Twitter</a>
+        </Col>
+      </Row>
+    </Container>
   )
 }
 
---
+++ src/components/tag-cloud.tsx
@@ -0,0 +1,37 @@
+import React from "react"
+import { useStaticQuery, graphql, Link } from "gatsby"
+
+import { flatten, identity, map, path, pipe, sortBy, uniq } from 'ramda'
+
+/**
+ * Find all tags used on the site.
+ * Sort alphabetically.
+ * TODO: relative font sizes.
+ */
+
+export default () => {
+  const data = useStaticQuery(graphql`
+    {
+      allMarkdownRemark {
+        nodes {
+          fields {
+            tags
+          }
+        }
+      }
+    }
+  `)
+
+  const onlyTags = map(path(['fields', 'tags']), data.allMarkdownRemark.nodes)
+  const tags = pipe(
+      flatten,
+      sortBy(identity),
+      uniq
+  )(onlyTags)
+
+  return (
+      <>
+        {map(tag => (<><Link to={`/tags/${tag}`}>{tag}</Link>{' '}</>), tags)}
+      </>
+  )
+}
